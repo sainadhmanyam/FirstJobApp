cc76a42bd20fc88ca22a40c9045c6dacfb988504
sainadhmanyam <76801368+sainadhmanyam@users.noreply.github.com>
2024-02-20 07:08:27 -0500

    spring boot practice application. creating a job portal app

---
+++ src/test/java/com/saispring/FirstJobApp/FirstJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saispring.FirstJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
